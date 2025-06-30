364dc7df5c0dc8afd132bdc0e9e743440c8c3c1a
Davi Leite <davi.conceicao@lightbase.com.br>
2025-06-30 15:03:31 -0300

    feat: future build with ListView

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_banco_douro/models/account.dart';
+import 'package:flutter_banco_douro/services/account_service.dart';
 import 'package:flutter_banco_douro/ui/styles/colors.dart';
+import 'package:flutter_banco_douro/ui/widgets/account_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,15 +11,46 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       backgroundColor: AppColors.lightGrey,
-       title: const Text("Sistema de gestão de contas"),
-       actions: [
-        IconButton(onPressed: (){
-          Navigator.pushReplacementNamed(context, "login");
-        }, 
-        icon: Icon(Icons.logout),
-        )
-       ],
+        backgroundColor: AppColors.lightGrey,
+        title: const Text("Sistema de gestão de contas"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, "login");
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: FutureBuilder(
+          future: AccountService().getAll(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.active:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.done:
+                {
+                  if (snapshot.data == null || snapshot.data!.isEmpty) {
+                    return Center(child: Text("Nenhuma conta recebida."));
+                  } else {
+                    List<Account> listAccounts = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: listAccounts.length,
+                      itemBuilder: (context, index) {
+                        return AccountWidget(account: listAccounts[index]);
+                      },
+                    );
+                  }
+                }
+            }
+          },
+        ),
       ),
     );
   }
